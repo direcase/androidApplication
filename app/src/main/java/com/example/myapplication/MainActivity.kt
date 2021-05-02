2cbb3d8fa9d809bc20792d0f6a44bac03b2669f2
майа хуху <dariya.bigabulova@gmail.com>
2021-05-02 15:39:29 +0600

    notification

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication
 
-import android.app.Dialog
+import android.app.*
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,9 +18,16 @@ import java.lang.Exception
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    var notificationChannel: NotificationChannel? = null
+    var notificationManager: NotificationManager? = null
+    var builder: Notification.Builder? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         val fileInputStream: FileInputStream
         try {
             fileInputStream=openFileInput("song1.txt")
@@ -53,10 +64,29 @@ class MainActivity : AppCompatActivity() {
                         e.printStackTrace()
                     }
                     dialog.hide()
+                    btnNotify()
                 }
 
             }
             dialog.show()
         }
+    }
+    fun btnNotify() {
+        val intent = Intent(this, LauncherActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager .IMPORTANCE_HIGH)
+            notificationChannel!!.lightColor = Color.BLUE
+            notificationChannel!!.enableVibration(true)
+            notificationManager!!.createNotificationChannel(notificationChannel!!)
+            builder = Notification.Builder(this, channelId).setContentTitle("NOTIFICATION USING " +
+                    "KOTLIN").setContentText("Successfully added text ").setSmallIcon(R.drawable.ic_launcher_background).setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable
+                    .ic_launcher_background)).setContentIntent(pendingIntent)
+        }
+        notificationManager!!.notify(12345, builder!!.build())
+    }
+    companion object {
+        const val channelId = "12345"
+        const val description = "You added text to song"
     }
 }
